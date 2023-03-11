9dfda0f6c85dc95f12557fc62de5c4bfb22be7cf
suniason <galvezbrett17@gmail.com>
2023-03-11 12:21:31 +0800

    Added codes for login and more

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,24 @@
 
+import Head from "next/head"
+import Link from "next/link"
 import React from "react"
-import Navbar from "./components/navbar"
 
 export default function Home() {
   return (
     <>
-      <Navbar/>
-      Index
+      <Head>
+        <title>BlogFeed</title>
+        <link rel="icon" href="title_logo.png" />
+      </Head>
+      <div>Some Random BS</div>
+        <Link href="/section/login">
+        <div className="bg-[#0063db] text-white p-3 rounded-full m-7 
+                          font-bold transition ease-in-out hover:-translate-y-1 
+                          hover:scale-110 hover:bg-blue-500 duration-300"
+        >Start Now 
+            <object data="arrow.svg" width="25" height="25" className="inline"></object>
+        </div>
+      </Link>
     </>
   )
 }
--- pages/section/login.tsx
+++ pages/section/login.tsx
@@ -1,7 +1,34 @@
+import Link from "next/link"
+import test from "node:test"
+
 const Login:React.FC = () => {
     return(
         <div>
-            Login
+            <div className="caret-transparent w-[50%] flex flex-col justify-center 
+            font-serif font-medium text-[#0063db] w-4/5">
+                <div className="flex justify-center m-10">
+                    <img src="/logo.png" alt="Logo" className="w-1/2"/>
+                </div>
+                <div className="flex justify-center text-2xl ">LOG IN</div>
+                <div className="flex justify-center flex-col text-1xl">
+                    <label htmlFor="uname">Username</label>
+                    <input type="text" name="uname" className="m-5 text-[#000000] outline outline-1 
+                    outline-[#0063db] rounded-full outline-offset-4 px-2"></input>
+                    <label htmlFor="pass">Password</label>
+                    <input type="text" name="pass" className="m-5 text-[#000000] outline outline-1 
+                    outline-[#0063db] rounded-full outline-offset-4 px-2"></input>
+                    <div className="my-2">No account yet? &nbsp;
+                        <Link href="/section/signup">
+                            <span className="underline cursor-pointer">Click Here</span>
+                        </Link>
+                    </div>
+                </div>
+                <div className="flex justify-center my-2">
+                <Link href="/section/homepage">
+                    <button className="bg-[#0063db] text-white w-1/5  rounded-full">LOG IN</button>
+                </Link>
+                </div>
+            </div>
         </div>
     )
 }
